35ac41f3ca4353f564c846ebd1e5d85d2969f59b
xwavex <xwavedw@gmail.com>
2016-07-27 10:28:46 +0200

    Added Cartesian Forces, Torques and Wrench types. fixes #2

---
+++ src/rst-rt/dynamics/Wrench.cpp
@@ -0,0 +1,51 @@
+/* ============================================================
+ *
+ * This file is a part of RST-RT (CogIMon) project
+ *
+ * Copyright (C) 2016 by Dennis Leroy Wigand <dwigand at cor-lab dot uni-bielefeld dot de>
+ *
+ * This file may be licensed under the terms of the
+ * GNU Lesser General Public License Version 3 (the ``LGPL''),
+ * or (at your option) any later version.
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the LGPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the LGPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/lgpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The development of this software was supported by:
+ *   CoR-Lab, Research Institute for Cognition and Robotics
+ *     Bielefeld University
+ *
+ * ============================================================ */
+
+#include "Wrench.hpp"
+
+namespace rstrt {
+namespace dynamics {
+
+Wrench::Wrench() {
+	this->forces.fill(0);
+	this->torques.fill(0);
+}
+
+Wrench::Wrench(double x, double y, double z, double a, double b, double c) :
+		forces(x, y, z), torques(a, b, c) {
+}
+
+std::ostream& operator<<(std::ostream& os, const Wrench& cd) {
+	return os << "Wrench : {\nforces : " << cd.forces << "\ntorques : "
+			<< cd.torques << "\n}";
+}
+
+std::istream& operator>>(std::istream& is, Wrench& cd) {
+	return is; // >> cd.angles;
+}
+
+}
+}
